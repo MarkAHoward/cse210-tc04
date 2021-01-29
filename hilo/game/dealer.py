15b7d74c7e88a5ec9888dfd16416426a042460ad
Mark <77358956+MarkAHoward@users.noreply.github.com>
2021-01-29 15:24:59 -0700

    Main Commit

--- hilo/game/dealer.py
+++ hilo/game/dealer.py
@@ -1,17 +1,20 @@
+import random
+
 class Dealer:
     def __init__(self):
-        
         self.card_old = 0
         self.card_new = 0
 
     def draw_card(self):
-        '''
-        Draws a random card and assign it to self.card_new and
-        self.card_new gets transfered to self.card_old
-        '''
+        card = random.randint(1, 13)
+        self.card_old = self.card_new
+        self.card_new = card
 
     def determine(self):
-        '''
-        Determines if new_card is > or < old_card and returns
-        'h' or 'l'.  If new_card == old_card returns 'n'
-        '''
+        if self.card_new > self.card_old:
+            result = 'h'
+        elif self.card_new < self.card_old:
+            result = 'l'
+        elif self.card_new == self.card_old:
+            result = 'n'
+        return result
--- hilo/game/director.py
+++ hilo/game/director.py
@@ -1,3 +1,5 @@
+from game.dealer import Dealer
+
 class Director:
     def __init__(self):
         self.points = 300
@@ -7,22 +9,26 @@ class Director:
     def start_game(self):
         '''
         Start the game
+        -Conner
         '''
     
     def get_choice(self):
         '''
         Ask user for 'h' or 'l' (self.choice)
+        -Allison
         '''
     
     def determine_points(self):
         '''
         Determine if self.choice < or > to result from dealer
         Assign or subtract points from self.points
+        -Allison
         '''
     
     def play_again(self):
         '''
         determine is self.points > 0 and if below or equal change
         self.alive to False
+        -Conner
         '''
 
